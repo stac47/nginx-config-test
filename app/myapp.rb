8cc1ab99c7f03e49f63b86dfaf9b00ee822da3a8
Laurent Stacul <laurent.stacul@onfido.com>
2023-12-08 18:35:11 +0100

    Nginx with more_headers module

--- app/myapp.rb
+++ app/myapp.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require 'sinatra'
+require "sinatra"
 
-get '/' do
+get "/:test_name" do
   request.env.each_with_object(String.new) do |(key, value), obj|
     obj << "#{key} = '#{value}'\n" if key.start_with?("HTTP_")
   end
